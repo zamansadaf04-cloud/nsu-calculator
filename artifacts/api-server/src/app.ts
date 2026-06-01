e3719c9886ffa73ca62f26fa0cb37d43aa56d5d2
zamansadaf04-cloud <zamansadaf04@gmail.com>
2026-06-01 11:50:05 +0600

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,7 +1,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
-import router from "./routes";
+import router from "./routes/index";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
